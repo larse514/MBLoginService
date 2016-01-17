e7e3ee20bed7ecdaafe2eca2fa6e30e5eec74d43
Andrew <andrew.larse514@gmail.com>
2016-01-17 15:33:28 -0600

    change to listening logic for OpenShift web server conventions

--- server.js
+++ server.js
@@ -40,8 +40,15 @@ app.use(function(req, res, next){
 //start the server
 //app.listen(process.env.PORT || 8080);
 // Start the server
-app.set('port', process.env.PORT || 8080);
+/*app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + server.address().port);
+});
+*/
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.listen(server_port, server_ip_address, function(){
+	console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
